4e82b447b86889a78f94d7ca44f4df0b6fa9ae9b
MaximMacarone <maximmacarone@gmail.com>
2025-01-16 02:35:03 +0300

    Initial Commit

---
+++ UIKitExperiments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitExperiments
+//
+//  Created by Maxim Makarenkov on 16.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
